6324416a92ee5dd1e41c1a3d54803929b842fb33
Dragos <edragos33@gmail.com>
2025-04-02 14:56:48 +0200

    email confirmation after sign up

--- backend/main/models.py
+++ backend/main/models.py
@@ -5,6 +5,7 @@ from unittest.util import _MAX_LENGTH
 from xml.dom import ValidationErr
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class Construction(models.Model):
     id = models.AutoField(primary_key=True)
@@ -26,3 +27,34 @@ class Construction(models.Model):
     def __str__(self):
         return f"{self.get_type_display()} at {self.address}"
         
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email,username, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email adress")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
